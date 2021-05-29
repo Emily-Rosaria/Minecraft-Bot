fa32b36ec683b5296ae14ebf8b13895db5768c50
Emily Rose <ebonywoodhellscythe@gmail.com>
2021-05-29 23:48:43 +0100

    Quick fix

--- events/on-delete.js
+++ events/on-delete.js
@@ -1,6 +1,4 @@
 const config = require('./../config.json'); // load bot config
-const mongoose = require("mongoose"); //database library
-const Users = require("./../database/models/users.js"); // users model
 
 module.exports = {
   name: "onDelete",
--- events/on-reaction-clear.js
+++ events/on-reaction-clear.js
@@ -1,5 +1,4 @@
 const config = require('./../config.json'); // load bot config
-const mongoose = require("mongoose"); //database library
 
 module.exports = {
   name: "onReactionClear",
